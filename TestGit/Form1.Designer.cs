aa393d91c87c16eb4a7f362834fa03726c286f13
Jimi Andro-Vasko <james.andro-vasko@unlv.edu>
2021-07-20 20:16:17 -0700

    Changed button name

--- TestGit/Form1.Designer.cs
+++ TestGit/Form1.Designer.cs
@@ -34,19 +34,21 @@ namespace TestGit
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(350, 131);
+            this.button1.Location = new System.Drawing.Point(700, 252);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 44);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "btnClickMe";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(626, 362);
+            this.ClientSize = new System.Drawing.Size(1252, 696);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
