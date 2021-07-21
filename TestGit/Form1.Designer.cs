0176a339fd22390c73dfb3b87fdf575eceaeadd7
Jimi Andro-Vasko <james.andro-vasko@unlv.edu>
2021-07-20 20:27:44 -0700

    Renamed button to diff

--- TestGit/Form1.Designer.cs
+++ TestGit/Form1.Designer.cs
@@ -38,7 +38,7 @@ namespace TestGit
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Diff";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
