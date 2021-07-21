af01b5cced0640685c015874d6f5f4025e9289d5
Jimi Andro-Vasko <james.andro-vasko@unlv.edu>
2021-07-20 20:06:23 -0700

    Resized again

--- TestGit/Form1.Designer.cs
+++ TestGit/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace TestGit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 198);
+            this.ClientSize = new System.Drawing.Size(626, 362);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
